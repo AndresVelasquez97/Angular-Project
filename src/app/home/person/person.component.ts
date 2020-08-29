764d2197bd28ddef708b7fe599a2c9d248fceeb6
AndresVelasquez97 <velasquezbotelloandres@gmail.com>
2020-08-29 10:31:37 -0500

    First Commit

---
+++ src/app/home/person/person.component.ts
@@ -0,0 +1,61 @@
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {PersonService} from './person.service';
+import {MatPaginator} from '@angular/material/paginator';
+import {merge, Observable} from 'rxjs';
+import {catchError, debounceTime, map, startWith, switchMap, tap} from 'rxjs/operators';
+import {FormControl} from '@angular/forms';
+import {MatSort} from '@angular/material/sort';
+
+@Component({
+    selector: 'app-person',
+    templateUrl: './person.component.html',
+    styleUrls: ['./person.component.scss']
+})
+export class PersonComponent implements AfterViewInit {
+    displayedColumns: string[] = ['identification', 'first_name', 'last_name', 'email', 'phone'];
+    data: string[] = [];
+    resultsLength = 0;
+    isLoadingResults = true;
+    isRateLimitReached = false;
+    searchValue = '';
+    inputSearch = new FormControl();
+    search: Observable<string>;
+
+    @ViewChild(MatPaginator) paginator: MatPaginator;
+    @ViewChild(MatSort) sort: MatSort;
+
+    deleteSearch(): void {
+        this.isLoadingResults = true;
+        this.inputSearch.setValue('');
+    }
+
+    constructor(private personService: PersonService) {
+    }
+
+    ngAfterViewInit(): void {
+        this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0);
+        this.search = this.inputSearch.valueChanges.pipe(
+            debounceTime(500),
+            tap(t => this.searchValue = t)
+        );
+        this.search.subscribe(() => this.paginator.pageIndex = 0);
+        merge(this.sort.sortChange, this.paginator.page, this.search)
+            .pipe(
+                startWith({}),
+                switchMap(() => {
+                    this.isLoadingResults = true;
+                    return this.personService.findAll(this.sort.direction, this.sort.active, this.paginator.pageIndex, this.searchValue);
+                }),
+                map(data => {
+                    this.isLoadingResults = false;
+                    this.resultsLength = data.count;
+                    return data.results;
+                }),
+                catchError(() => {
+                    this.isLoadingResults = false;
+                    this.isRateLimitReached = true;
+                    return ([]);
+                })
+            ).subscribe(data => this.data = data);
+    }
+}
